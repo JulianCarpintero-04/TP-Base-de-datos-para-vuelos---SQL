87b289d339939a5fbefc7360b1bb2fb4bfc55da8
Julian <jcarpintero@frba.utn.edu.ar>
2026-06-18 17:01:58 -0300

    Hacemos algunos inserts y creamos vistas

--- script_creacion_BI.sql
+++ script_creacion_BI.sql
@@ -19,19 +19,17 @@ CREATE TABLE BI_DimTiempo (
 
 CREATE TABLE BI_DimRangoEtario (
     id_Rango_Etario INT PRIMARY KEY,
-    anio_inicio INT,
-    anio_final INT
+    descripcion VARCHAR(50) --Acá pondríamos 'menores de 25 años',etc
 );
 
 CREATE TABLE BI_DimTemporada (
     id_Temporada INT PRIMARY KEY,
-    mes_inicio INT,
-    mes_final INT
+    descripcion VARCHAR(50) --Acá pondríamos 'Otoño','Invierno','Primavera','Verano'
 );
 
 CREATE TABLE BI_DimTipoServicio (
     id_Tipo_Servicio INT PRIMARY KEY,
-    descripcion VARCHAR(50)
+    descripcion VARCHAR(50) 
 );
 
 CREATE TABLE BI_DimCanalVenta (
@@ -86,4 +84,43 @@ CREATE TABLE BI_Hecho_Evaluacion(
   puntaje INT
 );
 
+INSERT INTO BI_DimTiempo VALUES ();
+INSERT INTO BI_DimRangoEtario VALUES ("Menores de 25 años"), ("Entre 25 y 35 anios"), ("Entre 35 y 50 anios"), ("Mayores de 50 anios");
+INSERT INTO BI_DimTemporada VALUES ("Verano"), ("Otonio"), ("Invierno"), ("Primavera");
+INSERT INTO BI_DimTipoSerivicio VALUES ("Venta Directa"), ("Propuesta a Medida");
+INSERT INTO BI_DimCanalVenta VALUES ("Telefono"), ("Whatsapp"), ("Mail"), ("Presencial");
+INSERT INTO BI_DimEstado VALUES ("Aceptado"), ("Rechazado");
+INSERT INTO BI_DimAspecto VALUES ("Claridad de informacion"), ("Efectividad"), ("Rapidez de respuesta"), ("Atencion del agente"), ("Satisfaccion general");
 
+INSERT INTO BI_Hecho_Evaluacion VALUES ();
+INSERT INTO BI_Hecho_Propuesta VALUES ();
+INSERT INTO BI_Hecho_Solicitud VALUES ();
+INSERT INTO BI_Hecho_Venta VALUES ();
+
+SELECT DISTINCT YEAR(fecha_nacimiento) AS Anio, MONTH(fecha_nacimiento) AS Mes FROM PLATENSE.Cliente
+UNION
+SELECT DISTINCT YEAR(fecha_nacimiento) AS Anio, MONTH(fecha_nacimiento) AS Mes FROM PLATENSE.Agente
+
+
+--------------------VIEWS--------------------
+CREATE VIEW BI_Ticket_Promedio AS
+SELECT T.a
+FROM BI_Hecho_Venta V JOIN BI_DimTiempo T ON
+
+CREATE VIEW BI_Distribución_Facturación AS
+
+CREATE VIEW BI_Ranking_Solicitudes_Por_Temporadas AS
+
+CREATE VIEW BI_Anticipación_Promedio_Solicitudes AS
+
+CREATE VIEW BI_Tasa_Aceptación_Propuestas AS
+
+CREATE VIEW BI_Cotización_Promedio_Por_Temporada AS
+
+CREATE VIEW BI_Tiempo_Promedio_Respuesta AS
+
+CREATE VIEW BI_Desvío_Presupuesto AS
+
+CREATE VIEW BI_Ranking_Aspectos_Valorados AS
+
+CREATE VIEW BI_Satisfacción_Promedio_Por_Agente AS
