c472f9f473164197f0753f6100cb7f7274059dfe
Juan Diego Cony <Jcony@frba.utn.edu.ar>
2026-06-18 16:11:09 -0300

    Pequeños cambios

--- script_creacion_BI.sql
+++ script_creacion_BI.sql
@@ -32,11 +32,17 @@ CREATE TABLE BI_DimEstado (
     descripcion VARCHAR(50)
 );
 
+CREATE TABLE BI_DimAspecto (
+    id_Aspecto INT PRIMARY KEY,
+    descripcion VARCHAR(50)
+);
+
 CREATE TABLE BI_Hecho_Venta(
     UniqueID INT IDENTITY(1,1) PRIMARY KEY,
     id_tiempo INT REFERENCES BI_DimTiempo(id_Tiempo),
     id_canal_venta INT REFERENCES BI_DimCanalVenta(id_Canal_Venta),
-    id_rango_etario INT REFERENCES BI_DimRangoEtario(id_Rango_Etario),
+    id_rango_etario_cliente INT REFERENCES BI_DimRangoEtario(id_Rango_Etario),
+    id_rango_etario_agente INT REFERENCES BI_DimRangoEtario(id_Rango_Etario),
     id_tipo_servicio INT REFERENCES BI_DimTipoServicio(id_Tipo_Servicio),
     precio_venta DECIMAL(18,2)
 );
@@ -45,7 +51,7 @@ CREATE TABLE BI_Hecho_Solicitud(
     UniqueID INT IDENTITY(1,1) PRIMARY KEY,
     id_tiempo INT REFERENCES BI_DimTiempo(id_Tiempo),
     id_temporada INT REFERENCES BI_DimTemporada(id_Temporada),
-    id_rango_etario INT REFERENCES BI_DimRangoEtario(id_Rango_Etario),
+    id_rango_etario_cliente INT REFERENCES BI_DimRangoEtario(id_Rango_Etario),
     dias_anticipacion INT
 );
 
@@ -54,23 +60,18 @@ CREATE TABLE BI_Hecho_Propuesta(
     id_tiempo INT REFERENCES BI_DimTiempo(id_Tiempo),
     id_estado INT REFERENCES BI_DimEstadoPropuesta(id_Estado_Propuesta),
     id_temporada INT REFERENCES BI_DimTemporada(id_Temporada),
-    id_rango_etario INT REFERENCES BI_DimRangoEtarioAgente(id_Rango_Etario),
+    id_rango_etario_agente INT REFERENCES BI_DimRangoEtario(id_Rango_Etario),
     importe DECIMAL(18,2),
     dias_transcurridos_emision INT,
     desvio_precio DECIMAL(18,2)
 );
 
-CREATE TABLE BI_Hecho_Encuesta(
+CREATE TABLE BI_Hecho_Evaluacion(
   id_hecho_encuesta INT PRIMARY KEY,
   id_tiempo INT REFERENCES BI_DimTiempo(id_Tiempo),
-  id_rango_etario INT REFERENCES BI_DimRangoEtarioAgente(id_Rango_Etario),
+  id_aspecto INT REFERENCES BI_DimAspecto(id_Aspecto),
+  id_rango_etario_agente INT REFERENCES BI_DimRangoEtario(id_Rango_Etario),
   puntaje INT
 );
 
-
-CREATE TABLE BI_Hecho_Aspecto(
-  id_hecho_aspecto INT PRIMARY KEY,
-  id_tiempo INT REFERENCES BI_DimTiempo(id_Tiempo),
-  puntaje INT
-)
 
